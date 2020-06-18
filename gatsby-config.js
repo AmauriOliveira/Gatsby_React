cfda5f360d52cfa4fbafa4f2024ab71f950a8fef
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-06-17 23:58:13 -0300

    add styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@Amauri Oliveira`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
