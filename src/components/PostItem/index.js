46340add38dbf427df2bd61352a5a6b3607952b2
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-07-04 22:38:27 -0300

    Add gatsby-transformer-remark

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,26 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import * as S from './styles';
 
-const PostItem = () => (
-    <S.PostItemLink to="/slug/">
-        <S.PostItemWrapper>
-            <S.PostItemTag background="#47650B">
-                AAO
-            </S.PostItemTag>
-            <S.PostItemInfo>
-                <S.PostItemDate>
-                    15 de Novembro
-                </S.PostItemDate>
-                <S.PostItemTitle>
-                    Amauri Oliveira
-                </S.PostItemTitle>
-                <S.PostItemDescription>
-                    Informação generica
-                </S.PostItemDescription>
-            </S.PostItemInfo>
-        </S.PostItemWrapper>
-    </S.PostItemLink>
-)
+const PostItem = ({
+    slug,
+    background,
+    category,
+    date,
+    title,
+    description }) => (
+        <S.PostItemLink to={slug}>
+            <S.PostItemWrapper>
+                <S.PostItemTag background={background}>
+                    {category}
+                </S.PostItemTag>
+                <S.PostItemInfo>
+                    <S.PostItemDate>
+                        {date}
+                    </S.PostItemDate>
+                    <S.PostItemTitle>
+                        {title}
+                    </S.PostItemTitle>
+                    <S.PostItemDescription>
+                        {description}
+                    </S.PostItemDescription>
+                </S.PostItemInfo>
+            </S.PostItemWrapper>
+        </S.PostItemLink>
+    )
+
+PostItem.propTypes = {
+    slug: PropTypes.string.isRequired,
+    background: PropTypes.string,
+    category: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+}
 
 export default PostItem;
